1e0431cd91d071786fa8631a906b8248277a97c1
avillaq <villafuertequispealex@gmail.com>
2024-06-30 18:43:51 -0500

    Se añade el modelo Pelicula

--- backend-django/api/models.py
+++ backend-django/api/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Pelicula(models.Model):
+    titulo = models.CharField(max_length=100)
+    descripcion = models.TextField()
+    fecha_estreno = models.DateField()
+    duracion = models.PositiveIntegerField()   # minutos
+    director = models.CharField(max_length=100)
+    poster_url = models.URLField(max_length=200)    # imagen
+    tmdb_id = models.PositiveIntegerField(unique=True)
+
+    def __str__(self):
+        return self.titulo
