00ec9506f66c6825eaa3adc6a26813e093693730
avillaq <villafuertequispealex@gmail.com>
2024-07-01 00:55:27 -0500

    Se modifica los endpoints para obtener las funciones de acuerdo a una pelicula

--- backend-django/api/urls.py
+++ backend-django/api/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
-from .views import PeliculaLista, PeliculaDetalle, FuncionLista, FuncionDetalle, BoletoLista, BoletoDetalle
+from .views import PeliculaLista, PeliculaDetalle, BoletoLista, BoletoDetalle, FuncionListaPorPelicula, FuncionDetallePorPelicula
 
 urlpatterns = [
     path('peliculas/', PeliculaLista.as_view(), name='pelicula-lista'),
     path('peliculas/<int:pk>/', PeliculaDetalle.as_view(), name='pelicula-detalle'),
-    path('funciones/', FuncionLista.as_view(), name='funcion-lista'),
-    path('funciones/<int:pk>/', FuncionDetalle.as_view(), name='funcion-detalle'),
+    path('peliculas/<int:pelicula_id>/funciones/', FuncionListaPorPelicula.as_view(), name='funcion-lista-por-pelicula'),
+    path('peliculas/<int:pelicula_id>/funciones/<int:pk>/', FuncionDetallePorPelicula.as_view(), name='funcion-detalle-por-pelicula'),
     path('boletos/', BoletoLista.as_view(), name='boleto-lista'),
     path('boletos/<int:pk>/', BoletoDetalle.as_view(), name='boleto-detail'),
 ]
