5cb0ad9107323a01dad6eb7f41f254b2c5bde555
avillaq <villafuertequispealex@gmail.com>
2024-06-30 23:58:01 -0500

    Creamos el modelo Funcion que tiene como llave foranea a Pelicula . Este modelo almacena el horario y la sala para ver la pelicula

--- backend-django/api/models.py
+++ backend-django/api/models.py
@@ -12,3 +12,11 @@ class Pelicula(models.Model):
 
     def __str__(self):
         return self.titulo
+
+class Funcion(models.Model):
+    pelicula = models.ForeignKey(Pelicula, on_delete=models.CASCADE)
+    horario = models.DateTimeField()
+    sala = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"{self.pelicula.titulo} - {self.horario}"
--- backend-django/api/urls.py
+++ backend-django/api/urls.py
@@ -3,6 +3,6 @@ from .views import PeliculaLista, PeliculaDetalle
 
 urlpatterns = [
     path('peliculas/', PeliculaLista.as_view(), name='pelicula-lista'),
-    path('peliculas/<int:id>/', PeliculaDetalle.as_view(), name='pelicula-detalle'),
+    path('peliculas/<int:pk>/', PeliculaDetalle.as_view(), name='pelicula-detalle'),
 ]
 
