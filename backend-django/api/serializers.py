9c02ba331e02bcad5db4cd931cfe45660bbd3e5d
avillaq <villafuertequispealex@gmail.com>
2024-07-01 00:05:23 -0500

    Se añade la serializacion para los modelos creados

--- backend-django/api/serializers.py
+++ backend-django/api/serializers.py
@@ -1,7 +1,17 @@
 from rest_framework import serializers
-from .models import Pelicula
+from .models import Pelicula, Funcion, Boleto
 
 class PeliculaSerializer(serializers.ModelSerializer):
     class Meta:
         model = Pelicula
+        fields = '__all__'
+
+class FuncionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Funcion
+        fields = '__all__'
+
+class BoletoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Boleto
         fields = '__all__'
