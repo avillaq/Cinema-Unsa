9419ccbbbfaf4362cd39e835a229b18de8b8e01b
avillaq <villafuertequispealex@gmail.com>
2024-06-30 20:10:19 -0500

    Añadimos las url respectivas para las vistas

--- backend-django/api/urls.py
+++ backend-django/api/urls.py
@@ -1,1 +1,8 @@
+from django.urls import path
+from .views import PeliculaLista, PeliculaDetalle
+
+urlpatterns = [
+    path('peliculas/', PeliculaLista.as_view(), name='pelicula-lista'),
+    path('peliculas/<int:id>/', PeliculaDetalle.as_view(), name='pelicula-detalle'),
+]
 
--- backend-django/api/views.py
+++ backend-django/api/views.py
@@ -4,10 +4,10 @@ from rest_framework import generics
 from .models import Pelicula
 from .serializers import PeliculaSerializer
 
-class PeliculaListCreate(generics.ListCreateAPIView):
+class PeliculaLista(generics.ListCreateAPIView):
     queryset = Pelicula.objects.all()
     serializer_class = PeliculaSerializer
 
-class PeliculaDetail(generics.RetrieveUpdateDestroyAPIView):
+class PeliculaDetalle(generics.RetrieveUpdateDestroyAPIView):
     queryset = Pelicula.objects.all()
     serializer_class = PeliculaSerializer
