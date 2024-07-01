f9c039bb4a9e62c007a1cb1d97a86321382f4101
avillaq <villafuertequispealex@gmail.com>
2024-06-30 20:07:35 -0500

    Añadimos las vistas PeliculaListCreate y PeliculaDetail . Estas vistas realizan las operacione basicas de un CRUD

--- backend-django/api/views.py
+++ backend-django/api/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import generics
+from .models import Pelicula
+from .serializers import PeliculaSerializer
+
+class PeliculaListCreate(generics.ListCreateAPIView):
+    queryset = Pelicula.objects.all()
+    serializer_class = PeliculaSerializer
+
+class PeliculaDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Pelicula.objects.all()
+    serializer_class = PeliculaSerializer
