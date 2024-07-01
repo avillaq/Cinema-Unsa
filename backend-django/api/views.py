ced6da63df2c0112720994522b8f8c58cce92db6
avillaq <villafuertequispealex@gmail.com>
2024-07-01 00:14:47 -0500

    Añadimos los serializaers de los modelos a la vista para hacer operaciones basicas CRUD

--- backend-django/api/views.py
+++ backend-django/api/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 
 from rest_framework import generics
-from .models import Pelicula
-from .serializers import PeliculaSerializer
+from .models import Pelicula, Funcion, Boleto
+from .serializers import PeliculaSerializer, FuncionSerializer, BoletoSerializer
 
 class PeliculaLista(generics.ListCreateAPIView):
     queryset = Pelicula.objects.all()
@@ -11,3 +11,19 @@ class PeliculaLista(generics.ListCreateAPIView):
 class PeliculaDetalle(generics.RetrieveUpdateDestroyAPIView):
     queryset = Pelicula.objects.all()
     serializer_class = PeliculaSerializer
+
+class FuncionListCreate(generics.ListCreateAPIView):
+    queryset = Funcion.objects.all()
+    serializer_class = FuncionSerializer
+
+class FuncionDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Funcion.objects.all()
+    serializer_class = FuncionSerializer
+
+class BoletoListCreate(generics.ListCreateAPIView):
+    queryset = Boleto.objects.all()
+    serializer_class = BoletoSerializer
+
+class BoletoDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Boleto.objects.all()
+    serializer_class = BoletoSerializer
