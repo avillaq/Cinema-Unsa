148e2a11731ac5d87d32977b805997a0b06e9be1
avillaq <villafuertequispealex@gmail.com>
2024-07-01 00:04:08 -0500

    Añadimos los modelos al panel del admin

--- backend-django/api/admin.py
+++ backend-django/api/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Pelicula
+from .models import Pelicula, Funcion, Boleto
+
 admin.site.register(Pelicula)
+admin.site.register(Funcion)
+admin.site.register(Boleto)
