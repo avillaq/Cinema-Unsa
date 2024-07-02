8593f867d60cb4b7fef3607af6a7153028397269
Angie Isasi <aisasi@unsa.edu.pe>
2024-07-02 11:38:33 -0500

    Detalles de una pelicula

--- frontend-angular/src/app/detalle/detalle.component.ts
+++ frontend-angular/src/app/detalle/detalle.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, Input,OnInit, numberAttribute } from '@angular/core';
+import { PeliculasService } from '../peliculas.service';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-detalle',
   standalone: true,
-  imports: [],
+  imports: [DatePipe],
   templateUrl: './detalle.component.html',
   styleUrl: './detalle.component.css'
 })
-export class DetalleComponent {
+export class DetalleComponent implements OnInit{
+  @Input("pelicula_id") pelicula_id: number = 0;
+  pelicula: any = {};
+  constructor(private PeliculaService: PeliculasService) {}
 
+  ngOnInit() {
+    this.PeliculaService.getPelicula(this.pelicula_id).subscribe(data => {
+      this.pelicula = data;
+    });
+    
+  }
 }
