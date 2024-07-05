98ab6645142a9ec2d490a960c07dd6e33b562487
avillaq <villafuertequispealex@gmail.com>
2024-07-05 18:32:43 -0500

    Añadimos modulos para que la plantilla stepper funcione y sea responsivo

--- frontend-angular/src/app/funcion-detalle/funcion-detalle.component.ts
+++ frontend-angular/src/app/funcion-detalle/funcion-detalle.component.ts
@@ -1,12 +1,33 @@
 import { Component } from '@angular/core';
+import {BreakpointObserver} from '@angular/cdk/layout';
+import {StepperOrientation, MatStepperModule} from '@angular/material/stepper';
+import {Observable} from 'rxjs';
+import {map} from 'rxjs/operators';
+import {MatButtonModule} from '@angular/material/button';
+import {AsyncPipe} from '@angular/common';
 
 @Component({
   selector: 'app-funcion-detalle',
   standalone: true,
-  imports: [],
+  imports: [
+    MatStepperModule,
+    MatButtonModule,
+    AsyncPipe
+  ],
   templateUrl: './funcion-detalle.component.html',
   styleUrl: './funcion-detalle.component.css'
 })
 export class FuncionDetalleComponent {
+
+
+  // Orientación del stepper
+  stepperOrientation: Observable<StepperOrientation>;
+
+  // Constructor que se encarga de la orientación del stepper: horizontal o vertical
+  constructor(breakpointObserver: BreakpointObserver, ) {
+    this.stepperOrientation = breakpointObserver
+      .observe('(min-width: 800px)')
+      .pipe(map(({matches}) => (matches ? 'horizontal' : 'vertical')));
+  }
 
 }
