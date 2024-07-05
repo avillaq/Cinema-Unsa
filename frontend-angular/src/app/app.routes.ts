aeee96563f14350f50f1c769d9f058b2a8b29573
Angie Isasi <aisasi@unsa.edu.pe>
2024-07-05 11:24:44 -0500

    configurando ruta de los botones

--- frontend-angular/src/app/app.routes.ts
+++ frontend-angular/src/app/app.routes.ts
@@ -6,4 +6,5 @@ export const appRoutes: Routes= [
   { path: '', component: PeliculasComponent },
   { path: 'peliculas', component: PeliculasComponent},
   { path: 'peliculas/:pelicula_id', component: DetalleComponent},
+  { path: 'peliculas/:pelicula_id/funciones/:funcion_id', component: DetalleComponent},
 ];
