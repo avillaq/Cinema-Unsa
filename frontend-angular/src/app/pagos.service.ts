22f599922b9abdbe14c4c7b9b358b2c137a21672
avillaq <villafuertequispealex@gmail.com>
2024-07-13 19:42:50 -0500

    Creamos el servicio que se va a encargar de los pagos

---
+++ frontend-angular/src/app/pagos.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { loadStripe } from '@stripe/stripe-js';
+import { map } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PagosService {
+  constructor( private http: HttpClient ) {}
+
+  private stripeAPIKey: any = 'pk_test_51PcEIjRpc8Awm1hPGYRiuEXxWy9iWO5pJgsBVQeGdSK8SyoxLZUiYJknlO4dv7d0mZzMpkYJ3MYDQTLpSzbTC9L100HLKAVBM8';
+
+  procesarPago(Datos: any) {
+    return this.http.post("http://127.0.0.1:8000/api/create-checkout-session/", Datos).pipe(
+      map(async (res: any) => {
+        const stripe = await loadStripe(this.stripeAPIKey);
+        stripe?.redirectToCheckout({
+          sessionId: res.id,
+        });
+      }
+    ));
+
+  }
+
+}
