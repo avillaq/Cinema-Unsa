c0fa6b8a7e80a08bec60108039ed59a7a95491a6
avillaq <villafuertequispealex@gmail.com>
2024-07-14 14:42:23 -0500

    Añadimos la ruta para la vista en la api

--- backend-django/api/urls.py
+++ backend-django/api/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import PeliculaLista, PeliculaDetalle, FuncionListaPorPelicula, FuncionDetallePorPelicula, BoletoListaPorFuncion, create_checkout_session, get_session_data
+from .views import PeliculaLista, PeliculaDetalle, FuncionListaPorPelicula, FuncionDetallePorPelicula, BoletoListaPorFuncion, create_checkout_session, get_session_data, generate_pdf
 urlpatterns = [
     path('peliculas/', PeliculaLista.as_view(), name='pelicula-lista'),
     path('peliculas/<int:pk>/', PeliculaDetalle.as_view(), name='pelicula-detalle'),
@@ -8,5 +8,6 @@ urlpatterns = [
     path('funciones/<int:funcion_id>/boletos/', BoletoListaPorFuncion.as_view(), name='boleto-lista-por-funcion'),
     path('create-checkout-session/', create_checkout_session, name='create-checkout-session'),
     path('get-session-data/<str:session_id>/', get_session_data, name='get-session-data'),
+    path('generar-recibo/', generate_pdf, name='generate_pdf'),
 ]
 
