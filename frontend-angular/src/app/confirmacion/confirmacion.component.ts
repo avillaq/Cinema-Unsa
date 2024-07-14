127bf90dc1d89266a5e613793ae455256e2b4575
avillaq <villafuertequispealex@gmail.com>
2024-07-14 18:24:52 -0500

    Preparamos los datos antes de enviarlos an backend

--- frontend-angular/src/app/confirmacion/confirmacion.component.ts
+++ frontend-angular/src/app/confirmacion/confirmacion.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { PagosService } from '../pagos.service';
 import { RouterLink } from '@angular/router';
+import { S } from '@angular/cdk/keycodes';
 @Component({
   selector: 'app-confirmacion',
   standalone: true,
@@ -11,14 +12,14 @@ import { RouterLink } from '@angular/router';
 export class ConfirmacionComponent implements OnInit{
   @Input("session_id") session_id: string = "";
   url_imagen: string = "../assets/img/confirmacion.png";
-
+  datos_pago: any = {};
   constructor(private pagosService:PagosService) { }
 
   ngOnInit() {
     this.pagosService.getDatosPago(this.session_id).subscribe(
       (data: any) => {
         if(data.status == "complete"){
-          console.log(data.customer_details.name);
+          this.datos_pago = data;
         }
       }
     );
@@ -33,6 +34,26 @@ export class ConfirmacionComponent implements OnInit{
       link.download = "Recibo_Compra.pdf";
       link.click();
       window.URL.revokeObjectURL(url);
+      console.log(this.filtrarDatosPago());
     });
+  }
+
+  filtrarDatosPago(): any {
+    let datos = {
+      nombre : this.datos_pago.customer_details.name,
+      email : this.datos_pago.customer_details.email,
+      dni : this.datos_pago.custom_fields[0].numeric.value,
+      telefono : this.datos_pago.customer_details.phone,
+
+      pelicula : this.datos_pago.metadata.pelicula,
+      sala : this.datos_pago.metadata.sala,
+      butacas : this.datos_pago.metadata.asientos,
+      fecha: this.datos_pago.metadata.fecha,
+      hora : this.datos_pago.metadata.hora,
+      boletos: JSON.parse(this.datos_pago.metadata.boletos.replace(/'/g, '"')),
+
+      monto: this.datos_pago.metadata.total,
+    };
+    return datos;
   }
 }
