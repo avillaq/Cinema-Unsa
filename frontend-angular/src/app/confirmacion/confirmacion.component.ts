4c1314c924038adbee0f6ac778612dc21be492b9
avillaq <villafuertequispealex@gmail.com>
2024-07-14 02:44:00 -0500

    En el componente de comfirmacion, hacemos uso del servicio para obtener los datos de la compra

--- frontend-angular/src/app/confirmacion/confirmacion.component.ts
+++ frontend-angular/src/app/confirmacion/confirmacion.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-
+import { Component, Input, OnInit } from '@angular/core';
+import { PagosService } from '../pagos.service';
 @Component({
   selector: 'app-confirmacion',
   standalone: true,
@@ -7,6 +7,19 @@ import { Component } from '@angular/core';
   templateUrl: './confirmacion.component.html',
   styleUrl: './confirmacion.component.css'
 })
-export class ConfirmacionComponent {
+export class ConfirmacionComponent implements OnInit{
+  @Input("session_id") session_id: string = "";
 
+  constructor(private pagosService:PagosService) { }
+
+  ngOnInit() {
+    this.pagosService.getDatosPago(this.session_id).subscribe(
+      (data: any) => {
+        if(data.status == "complete"){
+          alert("Pago completado con éxito");
+          console.log(data);
+        }
+      }
+    );
+  }
 }
