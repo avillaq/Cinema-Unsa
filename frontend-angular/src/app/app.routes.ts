c327532da7b719148b2c01eccf2a70fcae600174
avillaq <villafuertequispealex@gmail.com>
2024-07-14 00:37:43 -0500

    Añadimos la ruta de redireccion

--- frontend-angular/src/app/app.routes.ts
+++ frontend-angular/src/app/app.routes.ts
@@ -2,11 +2,12 @@ import { Routes } from '@angular/router';
 import { PeliculasComponent } from './peliculas/peliculas.component';
 import { DetalleComponent } from './detalle/detalle.component';
 import { FuncionDetalleComponent } from './funcion-detalle/funcion-detalle.component';
+import { ConfirmacionComponent } from './confirmacion/confirmacion.component';
 
 export const appRoutes: Routes= [
   { path: '', component: PeliculasComponent },
   { path: 'peliculas', component: PeliculasComponent},
   { path: 'peliculas/:pelicula_id', component: DetalleComponent},
   { path: 'peliculas/:pelicula_id/funciones/:funcion_id', component: FuncionDetalleComponent},
-  
+  { path: 'pago/confirmacion/', component: ConfirmacionComponent },
 ];
