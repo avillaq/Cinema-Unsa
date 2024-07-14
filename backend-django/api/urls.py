d3ffb9e52dfb3db02b488fca1aae4f66c3135e2e
avillaq <villafuertequispealex@gmail.com>
2024-07-13 20:04:45 -0500

    Añadimos la url de la vista

--- backend-django/api/urls.py
+++ backend-django/api/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import PeliculaLista, PeliculaDetalle, FuncionListaPorPelicula, FuncionDetallePorPelicula, BoletoListaPorFuncion
+from .views import PeliculaLista, PeliculaDetalle, FuncionListaPorPelicula, FuncionDetallePorPelicula, BoletoListaPorFuncion, create_checkout_session
 urlpatterns = [
     path('peliculas/', PeliculaLista.as_view(), name='pelicula-lista'),
     path('peliculas/<int:pk>/', PeliculaDetalle.as_view(), name='pelicula-detalle'),
     path('peliculas/<int:pelicula_id>/funciones/', FuncionListaPorPelicula.as_view(), name='funcion-lista-por-pelicula'),
     path('peliculas/<int:pelicula_id>/funciones/<int:pk>/', FuncionDetallePorPelicula.as_view(), name='funcion-detalle-por-pelicula'),
     path('funciones/<int:funcion_id>/boletos/', BoletoListaPorFuncion.as_view(), name='boleto-lista-por-funcion'),
+    path('create-checkout-session/', create_checkout_session, name='create-checkout-session'),
 ]
 
