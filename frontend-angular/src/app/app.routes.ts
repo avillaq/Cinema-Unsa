5761c937afd8eec15d60eb7f1f1d69735612a10c
Angie Isasi <aisasi@unsa.edu.pe>
2024-07-21 16:12:40 -0500

    Cambios de la pagina Web

--- frontend-angular/src/app/app.routes.ts
+++ frontend-angular/src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
 import { PeliculasComponent } from './peliculas/peliculas.component';
+import { CarouselComponent } from './carousel/carousel.component';
 import { DetalleComponent } from './detalle/detalle.component';
 import { FuncionDetalleComponent } from './funcion-detalle/funcion-detalle.component';
 
 export const appRoutes: Routes= [
   { path: '', component: PeliculasComponent },
+  { path: 'carousel', component: CarouselComponent },
   { path: 'peliculas', component: PeliculasComponent},
   { path: 'peliculas/:pelicula_id', component: DetalleComponent},
   { path: 'peliculas/:pelicula_id/funciones/:funcion_id', component: FuncionDetalleComponent},
