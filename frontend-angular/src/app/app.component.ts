70b524c8041d895ec0318f71a5152ff82faf5fcf
avillaq <villafuertequispealex@gmail.com>
2024-07-21 17:42:55 -0500

    Footer añadido

--- frontend-angular/src/app/app.component.ts
+++ frontend-angular/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavegacionComponent } from './navegacion/navegacion.component';
 import { PeliculasComponent } from './peliculas/peliculas.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavegacionComponent, PeliculasComponent],
+  imports: [RouterOutlet, NavegacionComponent, PeliculasComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
