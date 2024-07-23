1052ef06a70deb71c5bc439fa423b4eb5005dc74
avillaq <villafuertequispealex@gmail.com>
2024-07-22 21:44:12 -0500

    Configuramos el envio de correos

--- backend-django/backend/settings.py
+++ backend-django/backend/settings.py
@@ -136,3 +136,10 @@ CORS_ALLOWED_ORIGINS = [
 # Claves de stripe
 STRIPE_SECRET_KEY = "sk_test_51PcEIjRpc8Awm1hPpjc365C5vryPHq7HFd6xSwNQXc1oXx5bdGkjWyzMEWtXuF5cxIZsg2Qrt7tuebEkcTJXuiBr00o5N4bu7Y"
 STRIPE_PUBLIC_KEY = "pk_test_51PcEIjRpc8Awm1hPGYRiuEXxWy9iWO5pJgsBVQeGdSK8SyoxLZUiYJknlO4dv7d0mZzMpkYJ3MYDQTLpSzbTC9L100HLKAVBM8"
+
+# Configuración para enviar correos
+EMAIL_HOST = "smtp.googlemail.com"
+EMAIL_PORT = 587
+EMAIL_HOST_USER = "avillafuerteq@gmail.com"
+EMAIL_HOST_PASSWORD = "rbdg tbrb yfdd xeng"
+EMAIL_USE_TLS = True
