0b4360764b15ad7dc663f056b476927c66e7d4a6
Angie Isasi <aisasi@unsa.edu.pe>
2024-07-26 08:49:19 -0500

    imag

--- frontend-angular/src/app/app.routes.ts
+++ frontend-angular/src/app/app.routes.ts
@@ -1,17 +1,12 @@
 import { Routes } from '@angular/router';
 import { PeliculasComponent } from './peliculas/peliculas.component';
-<<<<<<< HEAD
 import { RankingComponent } from './ranking/ranking.component';
-=======
-import { CarouselComponent } from './carousel/carousel.component';
->>>>>>> Frontend
 import { DetalleComponent } from './detalle/detalle.component';
 import { FuncionDetalleComponent } from './funcion-detalle/funcion-detalle.component';
 import { ConfirmacionComponent } from './confirmacion/confirmacion.component';
 
 export const appRoutes: Routes= [
   { path: '', component: PeliculasComponent },
-  { path: 'carousel', component: CarouselComponent },
   { path: 'peliculas', component: PeliculasComponent},
   { path: 'peliculas/ranking', component: RankingComponent},
   { path: 'peliculas/:pelicula_id', component: DetalleComponent},
